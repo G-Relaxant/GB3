5a52c942c85aecae425e4fbd217729fbf03847fd
geekbrains <info@geekbrains.ru>
2022-06-15 20:58:51 +0400

    task4

---
+++ 4/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = Input("Введите число: ");
+Console.Write("Квадраты до вашего числа включительно: 1");
+int num = 1;
+while(num < number)
+{
+    Console.Write(", ");
+    num = num + 1;
+    int num2 = Quadro(num);
+    Console.Write(num2);
+}
+
+
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Quadro(int a)
+{
+    return a * a;
+}
